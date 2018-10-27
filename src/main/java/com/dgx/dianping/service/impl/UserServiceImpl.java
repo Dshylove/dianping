044464239c418f9b4190330398d85ef54c87259e
Dshylove <447161430@qq.com>
2018-10-27 22:18:59 +0800

    Update UserService

--- src/main/java/com/dgx/dianping/service/impl/UserServiceImpl.java
+++ src/main/java/com/dgx/dianping/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.dgx.dianping.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.BeanUtils;
@@ -11,20 +12,21 @@ import com.dgx.dianping.mapper.UserMapper;
 import com.dgx.dianping.pojo.User;
 import com.dgx.dianping.service.UserService;
 import com.dgx.dianping.util.CommonUtil;
+import com.dgx.dianping.util.MD5Util;
 
 @Service
 public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserMapper userMapper;
-	
+
 	@Override
 	public boolean validate(UserDto userDto) {
-		if(userDto != null && !CommonUtil.isEmpty(userDto.getName()) && !CommonUtil.isEmpty(userDto.getPassword())) {
+		if (userDto != null && !CommonUtil.isEmpty(userDto.getName()) && !CommonUtil.isEmpty(userDto.getPassword())) {
 			User user = new User();
 			BeanUtils.copyProperties(userDto, user);
 			List<User> list = userMapper.select(user);
-			if(list.size() == 1) {
+			if (list.size() == 1) {
 				BeanUtils.copyProperties(list.get(0), userDto);
 				return true;
 			}
@@ -34,32 +36,46 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public List<UserDto> getList() {
-		// TODO Auto-generated method stub
-		return null;
+		List<UserDto> result = new ArrayList<>();
+		List<User> userList = userMapper.select(new User());
+		for(User user : userList) {
+			UserDto userDto = new UserDto();
+			result.add(userDto);
+			BeanUtils.copyProperties(user, userDto);
+			userDto.setpId(0);
+		}
+		return result;
 	}
 
 	@Override
 	public boolean modify(UserDto userDto) {
-		// TODO Auto-generated method stub
-		return false;
+		User user = new User();
+		BeanUtils.copyProperties(userDto, user);
+		if(!CommonUtil.isEmpty(userDto.getPassword())) {
+			user.setPassword(MD5Util.getMD5(userDto.getPassword()));
+		}
+		return userMapper.update(user) == 1;
 	}
 
 	@Override
 	public boolean add(UserDto userDto) {
-		// TODO Auto-generated method stub
-		return false;
+//		User user = new User();
+//		BeanUtils.copyProperties(userDto, user);
+		userDto.setPassword(MD5Util.getMD5(userDto.getPassword()));
+		return userMapper.insert(userDto) == 1;
 	}
 
 	@Override
 	public boolean remove(Long id) {
-		// TODO Auto-generated method stub
-		return false;
+		return userMapper.delete(id) == 1;
 	}
 
 	@Override
 	public UserDto getById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		UserDto userDto = new UserDto();
+		User user = userMapper.selectById(id);
+		BeanUtils.copyProperties(user, userDto);
+		return userDto;
 	}
 
 }
